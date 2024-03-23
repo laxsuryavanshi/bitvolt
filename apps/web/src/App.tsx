24a2ec663f8949c8fdd0ed9bc869a9c65ea42b1c
Laxmikant Suryavanshi <laxmikantsuryavanshi@hotmail.com>
2024-03-24 00:07:44 +0530

    feat: added bare minimum code to enable authentication with aws-amplify and cognito user pool configuration

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,31 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Amplify } from 'aws-amplify'
+import { Authenticator, Button, Heading, Image, Text, View, useTheme } from '@aws-amplify/ui-react'
+
+import '@aws-amplify/ui-react/styles.css'
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.VITE_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+    },
+  },
+})
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <Authenticator loginMechanisms={['email']} hideSignUp components={components}>
+      {({ signOut, user }) => (
+        <View as="main">
+          <Text>Hello {user?.signInDetails?.loginId}</Text>
+          <Button size="small" onClick={signOut}>
+            Sign out
+          </Button>
+        </View>
+      )}
+    </Authenticator>
   )
+}
+
+const components = {
+  Header() {
+    const { tokens } = useTheme()
+
+    return (
+      <View padding={tokens.space.large} className="flex items-center justify-center -ml-8">
+        <Image alt="BitVolt" src="/photos-96.png" />
+        <View>
+          <Heading level={3}>BitVolt</Heading>
+          <Text fontSize={tokens.fontSizes.small} fontWeight={tokens.fontWeights.bold}>
+            turtleby
+          </Text>
+        </View>
+      </View>
+    )
+  },
+
+  Footer() {
+    const { tokens } = useTheme()
+
+    return (
+      <View padding={tokens.space.large} className="text-center">
+        <Text color={tokens.colors.neutral[80]}>&copy; All Rights Reserved</Text>
+      </View>
+    )
+  },
+
+  SignIn: {
+    Header() {
+      const { tokens } = useTheme()
+
+      return (
+        <Heading padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`} level={5}>
+          Sign in to your account
+        </Heading>
+      )
+    },
+
+    Footer() {
+      return <View className="pb-4 px-8"></View>
+    },
+  },
 }
 
 export default App
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@fontsource/open-sans/400.css'
+import '@fontsource/open-sans/500.css'
+import '@fontsource/open-sans/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
