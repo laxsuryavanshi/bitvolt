c2ab1e26c3216110cb45a990cc989cb5f89efd7d
Laxmikant Suryavanshi <laxmikantsuryavanshi@hotmail.com>
2024-03-26 13:12:30 +0530

    fix: tailwind base overriding amplify UI theme styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from './App.tsx'
 import '@fontsource/open-sans/400.css'
 import '@fontsource/open-sans/500.css'
 import '@fontsource/open-sans/700.css'
